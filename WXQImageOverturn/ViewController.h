a8b09be5fb0df0a025e39d1acf0c41584daee881
wangxueqi <wangxueqi1@100tal.com>
2019-06-28 16:55:20 +0800

    Initial Commit

---
+++ WXQImageOverturn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WXQImageOverturn
+//
+//  Created by WXQ on 2019/6/28.
+//  Copyright © 2019 WXQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WXQImageOverturnTests/WXQImageOverturnTests.m
@@ -0,0 +1,37 @@
+//
+//  WXQImageOverturnTests.m
+//  WXQImageOverturnTests
+//
+//  Created by WXQ on 2019/6/28.
+//  Copyright © 2019 WXQ. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WXQImageOverturnTests : XCTestCase
+
+@end
+
+@implementation WXQImageOverturnTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
